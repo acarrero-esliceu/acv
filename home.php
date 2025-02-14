eaf381b94879453a24b7253df82e7f31a3659e9d
acarrerov <acarrerov@esliceu.net>
2025-02-14 09:19:06 +0100

    paginacion

--- home.php
+++ home.php
@@ -31,177 +31,62 @@
                 </div>
             </div>
         </div>
-        <div class="container p-default2">
-            <div class="row align-items-start g-5 justify-content-center">
-                <div class="col-md-5 text-left">
-                    <img src="img/Imagen15.png" class="img-fluid" alt="">
-                    <div class="mt-4">
-                        <div class="row">
-                            <div class="col-4">
-                                <button class="btn btn-outline-info boton rounded-pill">culture</button>
-                            </div>
-                        </div>
-                        <div class="mt-3 texto-gris">
-                            <p>Mathias Reding | Game Boost | 6 min read</p>
-                            <p>December 20 ,2022</p>
-                        </div>
-                    </div>
-                    <div class="titulo">
-                        <h3><a class="text-decoration-none link-dark" href="post.html">Introducing Anima + Lottie</a></h3>
-                    </div>
-                    <div class="col-80">
-                        <p class="descripcion">How to add a Lottie animation to a prototype in Sketch, Adobe XD or Figma</p>
-                    </div>
+        <section>
+    <div class="row">
+
+        <?php
+        if (have_posts()) {
+            global $wp_query;
+
+
+            the_posts_pagination(array(
+                'mid_size' => 2,
+                'prev_text' => __( 'Anterior', 'textdomain'),
+                'next_text' => __( 'Siguiente', 'textdomain'),
+            ));
+
+
+            while (have_posts()) {
+                the_post();
+        ?>
+
+                <div class="col-lg-6 mb-5">
+                    <picture class="thumbnail">
+                        <?php
+                        if (has_post_thumbnail()) {
+                            the_post_thumbnail('medium', array('class' => 'rounded w-100'));
+                        } else {
+                            echo '<p>Sense imatge en el post</p>';
+                        }
+                        ?>
+                    </picture>
+
+                    <a class="new-title" href="<?php the_permalink(); ?>">
+                        <p><?php echo the_title(); ?></p>
+                    </a>
+
+                    <p class="subtitle mb-sm-2">
+                        <?php the_excerpt(); ?>
+                    </p>
                 </div>
-                <div class="col-md-5 text-left">
-                    <img src="img/Imagen11.png" class="img-fluid" alt="">
-                    <div class="mt-4">
-                        <div class="row">
-                            <div class="col-4">
-                                <button class="btn btn-outline-info boton rounded-pill">culture</button>
-                            </div>
-                        </div>
-                        <div class="mt-3 texto-gris">
-                            <p>Sarah Dorweiler | 3D Artist | 4 min read</p>
-                            <p>May 24,2022</p>
-                        </div>
-                    </div>
-                    <div class="titulo">
-                        <h3>Better Forms with Input Types, Auto-Complete, and Validation</h3>
-                    </div>
-                    <div class="col-80">
-                        <p class="descripcion">Anima allows designers to create high-fidelity prototypes inside Sketch, Adobe XD, and Figma and export HTML & CSS in a single click.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="container p-default2">
-            <div class="row align-items-start g-5 border-top border-light-subtle border-3 padingtop justify-content-center">
-                <div class="col-12 text-left">
-                    <h2 class="titulo">Trending topics</h2>
-                </div>
-                <div class="col-md-5 text-left">
-                    <img src="img/Imagen12.png" class="img-fluid" alt="">
-                    <div class="mt-4">
-                        <div class="row">
-                            <div class="col-4">
-                                <button class="btn btn-outline-info boton rounded-pill">culture</button>
-                            </div>
-                        </div>
-                        <div class="mt-3 texto-gris">
-                            <p>Stephen Leonardi | Software Engineer | 4 min read</p>
-                            <p>September 8 ,2022</p>
-                        </div>
-                    </div>
-                    <div class="titulo">
-                        <h3>Learn how to organize Your Anima Project with Sections</h3>
-                    </div>
-                    <div class="col-80">
-                        <p class="descripcion">Create dividers and drag screens to arrange your Anima projects.</p>
-                    </div>
-                </div>
-                <div class="col-md-5 text-left">
-                    <img src="img/Imagen13.png" class="img-fluid" alt="">
-                    <div class="mt-4">
-                        <div class="row">
-                            <div class="col-4">
-                                <button class="btn btn-outline-info boton rounded-pill">culture</button>
-                            </div>
-                        </div>
-                        <div class="mt-3 texto-gris">
-                            <p>Kate Glotova | Art Director | 5 min read</p>
-                            <p>March 13 ,2022</p>
-                        </div>
-                    </div>
-                    <div class="titulo">
-                        <h3>Anima Introduces: the best Hints you should know</h3>
-                    </div>
-                    <div class="col-80">
-                        <p class="descripcion">Hotspot Hints Help Guide Users Navigate Through a Prototype, Anima allows designers to create prototypes.</p>
-                    </div>
-                </div>
-                <div class="col-md-5 text-left">
-                    <img src="img/imagen9.png" class="img-fluid" alt="">
-                    <div class="mt-4">
-                        <div class="row">
-                            <div class="col-4">
-                                <button class="btn btn-outline-info boton rounded-pill">culture</button>
-                            </div>
-                        </div>
-                        <div class="mt-3 texto-gris">
-                            <p>Jessica Cordoba | Frontend Developer | 5 min read</p>
-                            <p>July 4 ,2022</p>
-                        </div>
-                    </div>
-                    <div class="titulo">
-                        <h3>Introducing Anima + Figma</h3>
-                    </div>
-                    <div class="col-80">
-                        <p class="descripcion">How to add a Lottie animation to a prototype in Sketch, Adobe XD or Figma</p>
-                    </div>
-                </div>
-                <div class="col-md-5 text-left">
-                    <img src="img/imagen10.png" class="img-fluid" alt="">
-                    <div class="mt-4">
-                        <div class="row">
-                            <div class="col-4">
-                                <button class="btn btn-outline-info boton rounded-pill">culture</button>
-                            </div>
-                        </div>
-                        <div class="mt-3 texto-gris">
-                            <p>Lorenzo Hamers | Product designer | 2 min read</p>
-                            <p>October 13,2022</p>
-                        </div>
-                    </div>
-                    <div class="titulo">
-                        <h3>Better Forms with Input Types, Auto-Complete, and Validation</h3>
-                    </div>
-                    <div class="col-80">
-                        <p class="descripcion">Anima allows designers to create high-fidelity prototypes inside all design tools.</p>
-                    </div>
-                </div>
-                <div class="col-md-5 text-left">
-                    <img src="img/imagen4.png" class="img-fluid" alt="">
-                    <div class="mt-4">
-                        <div class="row">
-                            <div class="col-4">
-                                <button class="btn btn-outline-info boton rounded-pill">culture</button>
-                            </div>
-                        </div>
-                        <div class="mt-3 texto-gris">
-                            <p>Kate Glotova | Art Director | 4 min read</p>
-                            <p>September 23 ,2022</p>
-                        </div>
-                    </div>
-                    <div class="titulo">
-                        <h3>Talking now about sharing!</h3>
-                    </div>
-                    <div class="col-80">
-                        <p class="descripcion">Typography in advertising often reflects a company's brand. A brand may use typography to express its theme, personality, and message.</p>
-                    </div>
-                </div>
-                <div class="col-md-5 text-left">
-                    <img src="img/Imagen14.png" class="img-fluid" alt="">
-                    <div class="mt-4">
-                        <div class="row">
-                            <div class="col-4">
-                                <button class="btn btn-outline-info boton rounded-pill">culture</button>
-                            </div>
-                        </div>
-                        <div class="mt-3 texto-gris">
-                            <p>Jessica Cordoba | Frontend Developer | 4 min read</p>
-                            <p>May 24,2022</p>
-                        </div>
-                    </div>
-                    <div class="titulo">
-                        <h3>What you need to know about code languages</h3>
-                    </div>
-                    <div class="col-80">
-                        <p class="descripcion">Rather than having to jump into strict syntax rules, Python reads like English and is simple to understand for someone who's new to programming.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
+
+        <?php
+            }
+
+        the_posts_pagination(array(
+            'mid_size' => 2,
+            'prev_text' => __( 'Anterior', 'textdomain'),
+            'next_text' => __( 'Siguiente', 'textdomain'),
+        ));
+
+
+        } else {
+            echo '<p>No hi ha posts disponibles</p>';
+        }
+        ?>
+
+    </div>
+</section>
         <div class="text-center mx-auto p-default2">
             
             <div class="row">
