b6e5f1bf73d1bf2477904e21a8a7f8d407b1a662
acarrerov <acarrerov@esliceu.net>
2025-02-20 13:18:37 +0100

    arreglado las columnas

--- front-page.php
+++ front-page.php
@@ -7,75 +7,76 @@
                 <div class="col-12 text-center mt-5 ps-4 title-description">
                     <img class="mt-4 mb-5 align-items-center img-fluid" src="img/welcome.png" alt="">
                 </div>
-                    <div class="col-12 text-start">
-                        <div class="row">
-                            <div class="col-2">
-                                <button class="btn btn-outline-info boton rounded-pill">technology</button>
-                            </div>
-                        </div>
-                        <div class="mt-3 texto-gris">
-                            <p>Kate Glotova | Art Director | 5 min read</p>
-                            <p>March 13, 2022</p>
+                <div class="col-12 text-start">
+                    <div class="row">
+                        <div class="col-2">
+                            <button class="btn btn-outline-info boton rounded-pill">technology</button>
                         </div>
                     </div>
-                    <div class="col-12 text-start">
-                        <div class="titulo">
-                            <h3>Anima Introduces: Hotspots Hints</h3>
-                        </div>
-                        <div class="descripcion col-80 mx-auto">
-                            <p>Write the blog description right here, Write the blog description right here, Write the blog description right here.Write the blog description right here, Write the blog description right here</p>
-                        </div>
-                        <div>
-                            <p><a class="link-opacity-100 link-underline link-underline-opacity-25" href="#">Learn more</a></p>
-                        </div>
+                    <div class="mt-3 texto-gris">
+                        <p>Kate Glotova | Art Director | 5 min read</p>
+                        <p>March 13, 2022</p>
+                    </div>
+                </div>
+                <div class="col-12 text-start">
+                    <div class="titulo">
+                        <h3>Anima Introduces: Hotspots Hints</h3>
+                    </div>
+                    <div class="descripcion col-80 mx-auto">
+                        <p>Write the blog description right here, Write the blog description right here, Write the blog description right here.Write the blog description right here, Write the blog[...]
+                    </div>
+                    <div>
+                        <p><a class="link-opacity-100 link-underline link-underline-opacity-25" href="#">Learn more</a></p>
                     </div>
                 </div>
             </div>
-            <div class="container mx-auto p-default2">
-                <div class="row align-items-start g-5 justify-content-center">
-                    <div class="col-md-5">
-                        <img src="img/Imagen1.png" class="img-fluid" alt="">
-                        <div class="row">
-                            <div class="col-4">
-                                <button class="btn btn-outline-info boton rounded-pill mt-4">ideas</button>
-                            </div>
-                        </div>
-                        <div class="mt-3 texto-gris">
-                            <p>Lorenzo Hamers | Product designer</p>
-                            <p>4 min read | October 13,2022</p>
-                        </div>
-                        <div class="titulo">
-                            <h3><a class="text-decoration-none link-dark" href="post.html">Better Forms with Input Types, Auto-Complete, and Validation</a></h3>
-                        </div>
-                        <div class="col-80">
-                            <p class="descripcion">Anima allows designers to create high-fidelity prototypes inside all design tools.</p>
+        </div>
+        <div class="container mx-auto p-default2">
+            <div class="row align-items-start g-5 justify-content-center">
+                <div class="col-md-5">
+                    <img src="img/Imagen1.png" class="img-fluid" alt="">
+                    <div class="row">
+                        <div class="col-4">
+                            <button class="btn btn-outline-info boton rounded-pill mt-4">ideas</button>
                         </div>
                     </div>
-                    <div class="col-md-5">
-                        <img src="img/imagen2.png" class="img-fluid" alt="">
-                        <div class="row">
-                            <div class="col-4">
-                                <button class="btn btn-outline-info boton rounded-pill mt-4">ideas</button>
-                            </div>
+                    <div class="mt-3 texto-gris">
+                        <p>Lorenzo Hamers | Product designer</p>
+                        <p>4 min read | October 13,2022</p>
+                    </div>
+                    <div class="titulo">
+                        <h3><a class="text-decoration-none link-dark" href="post.html">Better Forms with Input Types, Auto-Complete, and Validation</a></h3>
+                    </div>
+                    <div class="col-80">
+                        <p class="descripcion">Anima allows designers to create high-fidelity prototypes inside all design tools.</p>
+                    </div>
+                </div>
+                <div class="col-md-5">
+                    <img src="img/imagen2.png" class="img-fluid" alt="">
+                    <div class="row">
+                        <div class="col-4">
+                            <button class="btn btn-outline-info boton rounded-pill mt-4">ideas</button>
                         </div>
-                        <div class="mt-3 texto-gris">
-                            <p>Mathias Reding | Game Boost | 4 min read</p>
-                            <p>November 11 ,2022</p>
-                        </div>
-                        <div class="titulo">
-                            <h3>Anima Introduces: the best Hints you should know</h3>
-                        </div>
-                        <div class="col-80">
-                            <p class="descripcion">Hotspot Hints Help Guide Users Navigate Through a Prototype, Anima allows designers to create prototypes.</p>
-                        </div>
+                    </div>
+                    <div class="mt-3 texto-gris">
+                        <p>Mathias Reding | Game Boost | 4 min read</p>
+                        <p>November 11 ,2022</p>
+                    </div>
+                    <div class="titulo">
+                        <h3>Anima Introduces: the best Hints you should know</h3>
+                    </div>
+                    <div class="col-80">
+                        <p class="descripcion">Hotspot Hints Help Guide Users Navigate Through a Prototype, Anima allows designers to create prototypes.</p>
                     </div>
                 </div>
             </div>
-            <div class="container mx-auto p-default2">
-                <div class="row align-items-start g-5 border-top border-light-subtle border-3 padingtop justify-content-center">
-                    <div class="col-12 text-left">
-                        <h2 class="titulo">Trending topics</h2>
-                    </div>
+        </div>
+        <div class="container mx-auto p-default2">
+            <div class="row align-items-start g-5 border-top border-light-subtle border-3 padingtop justify-content-center">
+                <div class="col-12 text-left">
+                    <h2 class="titulo">Trending topics</h2>
+                </div>
+                <div class="row justify-content-center">
                     <div class="col-md-5">
                         <img src="img/imagen3.png" class="img-fluid" alt="">
                         <div class="row">
@@ -87,16 +88,6 @@
                             <p>Lorenzo Hamers | Product designer | 5 min read</p>
                             <p>March 13 ,2022</p>
                         </div>
-                        <div class="titulo">
-                            <h3>Better Forms with Input Types, Auto-Complete, and Validation</h3>
-                        </div>
-                        <div class="col-80">
-                            <p class="descripcion">Hotspot Hints Help Guide Users Navigate Through a Prototype, Anima allows designers to create prototypes.</p>
-                        </div>
-                    </div>
-                    <div class="col-md-5">
-                        <img src="img/imagen4.png" class="img-fluid" alt="">
-                        <div >
                         <div class="titulo">
                             <h3>Better Forms with Input Types, Auto-Complete, and Validation</h3>
                         </div>
@@ -122,6 +113,8 @@
                             <p class="descripcion">Typography in advertising often reflects a company's brand. A brand may use typography to express its theme, personality, and message.</p>
                         </div>
                     </div>
+                </div>
+                <div class="row justify-content-center">
                     <div class="col-md-5">
                         <img src="img/imagen5.png" class="img-fluid" alt="">
                         <div class="row">
@@ -158,6 +151,8 @@
                             <p class="descripcion">Rather than having to jump into strict syntax rules, Python reads like English and is simple to understand for someone who's new to programming.</p>
                         </div>
                     </div>
+                </div>
+                <div class="row justify-content-center">
                     <div class="col-md-5">
                         <img src="img/imagen7.png" class="img-fluid" alt="">
                         <div class="row">
@@ -196,11 +191,12 @@
                     </div>
                 </div>
             </div>
-            <div class="text-center mx-auto p-default2 align-items-center">
-                <div class="row">
-                    <div class="col-2 mucho-margin">
-                        <button class="btn botongrande btn-primary boton mt-4 btn-lg showmore">SHOW MORE</button>
-                    </div>
+        </div>
+        <div class="text-center mx-auto p-default2 align-items-center">
+            <div class="row">
+                <div class="col-2 mucho-margin">
+                    <button class="btn botongrande btn-primary boton mt-4 btn-lg showmore">SHOW MORE</button>
                 </div>
             </div>
+        </div>
 <?php get_footer() ?>
